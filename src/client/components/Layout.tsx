1a1731cb952611f2e0a9366f283b8612ed41cfc0
NumNine9 <gnyagura57@gmail.com>
2025-01-09 09:43:37 +0100

    Google auth setup

---
+++ src/client/components/Layout.tsx
@@ -0,0 +1,12 @@
+// A layout component to center the content
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="h-full w-full bg-white">
+        <div className="flex min-h-[75vh] min-w-full items-center justify-center">
+          <div className="h-full w-full max-w-sm bg-white p-5">
+            <div>{children}</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
